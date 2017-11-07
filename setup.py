beb342ff1d1a3c6b16211795484779994c154c64
ruffsl <roxfoxpox@gmail.com>
2017-11-07 15:54:32 -0800

    Adding base package files

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='keymint_package',
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    author='Ruffin White',
+    author_email='ruffin@osrfoundation.org',
+    maintainer='Ruffin White',
+    maintainer_email='ruffin@osrfoundation.org',
+    url='https://github.com/keymint/keymint_package/wiki',
+    download_url='https://github.com/keymint/keymint_package/releases',
+    keywords=['ROS'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description='Keymint is a build system for keystores.',
+    long_description="""\
+Keymint defines metainformation for keys, their certificats,
+and provides tooling to build these federated keystores together.""",
+    license='Apache License, Version 2.0',
+    test_suite='test',
+    # package_data={
+    #     'keymint_package': [
+    #         'template/environment_hook/*',
+    #         'template/isolated_prefix_level/*',
+    #         'template/package_level/*',
+    #         'template/prefix_level/*',
+    #     ],
+    # },
+)
