b75b0a7de384b6cdb00f6e89b9e58a7c4b834321
ruffsl <roxfoxpox@gmail.com>
2017-11-15 20:06:13 -0800

    Staging working identity singing

--- keymint_package/identities.py
+++ keymint_package/identities.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 import os
+import datetime
 
 import xml.etree.ElementTree as ElementTree
 
@@ -43,7 +44,9 @@ class DDSCertificateHelper:
     """Help build certificate into artifacts."""
 
     def build_csr(self, context, csr, dds_key):
-        # TODO use data in csr xml element to provide subject name
+        # FIXME use data in csr xml element to provide subject name
+        # OMG Secure DDS 9.4.1.3.2.1 Subject name Section
+        # https://tools.ietf.org/html/rfc4514
         hash_algorithm = getattr(hashes, csr.find('hash_algorithm').text)
         dds_csr = x509.CertificateSigningRequestBuilder().subject_name(x509.Name([
             # Provide various details about who we are.
@@ -63,6 +66,56 @@ class DDSCertificateHelper:
             # Sign the CSR with our private key.
             ).sign(dds_key, hash_algorithm(), default_backend())
         return dds_csr
+
+    def get_ca(self, context, issuer_name):
+        # TODO conceder getting ca password from context
+        issuer_name = os.path.normpath(issuer_name)
+        ca_key_path = os.path.join(context.private_space, issuer_name + '.pem')
+        ca_cert_path = os.path.join(context.public_space, issuer_name + '.pem')
+        print('ca_key_path: ', ca_key_path)
+
+        with open(ca_key_path, 'rb') as f:
+            ca_key = serialization.load_pem_private_key(
+                f.read(),
+                password=None,
+                backend=default_backend())
+
+        with open(ca_cert_path, 'rb') as f:
+            ca_cert = x509.load_pem_x509_certificate(f.read(), default_backend())
+
+        return ca_key, ca_cert
+
+    def install_cert(self, context, cert, dds_csr):
+        # TODO shouldn't choice of ca hash_algorithm come from ca context?
+        hash_algorithm = getattr(hashes, cert.find('hash_algorithm').text)()
+
+        subject = dds_csr.subject
+
+        serial_number = int(cert.find('serial_number').text)
+
+        validity = cert.find('validity')
+        not_before = validity.find('not_before').text
+        not_before_datetime = datetime.datetime.strptime(not_before, '%Y-%m-%dT%H:%M:%S')
+        not_after = validity.find('not_after').text
+        not_after_datetime = datetime.datetime.strptime(not_after, '%Y-%m-%dT%H:%M:%S')
+
+        ca_key, ca_cert = self.get_ca(context, cert.find('issuer_name').text)
+
+        cert_builder = x509.CertificateBuilder().subject_name(
+            subject
+        ).serial_number(
+            serial_number
+        ).not_valid_before(
+            not_before_datetime
+        ).not_valid_after(
+            not_after_datetime
+        ).public_key(
+            dds_csr.public_key()
+        ).issuer_name(
+            ca_cert.subject
+        )
+        dds_cert = cert_builder.sign(ca_key, hash_algorithm, default_backend())
+        return dds_cert
 
     def serialize(self, context, cert, dds_certificate):
         dds_certificate_bytes = dds_certificate.public_bytes(
@@ -168,6 +221,25 @@ class DDSIdentitiesHelper(IdentitiesHelper):
             dds_identities.append(dds_identity)
 
         return dds_identities
+
+    def _install_identity(self, context, identity, dds_identity):
+        cert = identity.find('cert')
+
+        dds_csr_bytes = dds_identity['dds_csr']['bytes']
+        dds_csr = x509.load_pem_x509_csr(dds_csr_bytes, default_backend())
+
+        dds_cert = self.dds_certificate_helper.install_cert(context, cert, dds_csr)
+        dds_cert_bytes = self.dds_certificate_helper.serialize(context, cert, dds_cert)
+
+        dds_identity['dds_csr'] = {'object': dds_csr, 'bytes': dds_csr_bytes}
+        dds_identity['dds_cert'] = {'object': dds_cert, 'bytes': dds_cert_bytes}
+
+        return dds_identity
+
+    def install(self, context, dds_identity):
+        identity = deepcopy(context.package_manifest.identities.findall('identity')[0])
+        dds_identity = self._install_identity(context, identity, dds_identity)
+        return dds_identity
 
     # def test(self, dds_root_str, filename):
     #     permissions_xsd_path = get_dds_template_path('permissions.xsd')
