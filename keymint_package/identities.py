9ad9254891efac725fdc750f5adca05602378b49
ruffsl <roxfoxpox@gmail.com>
2017-11-16 00:56:32 -0800

    Fixing use of subject name from xml cert element

--- keymint_package/identities.py
+++ keymint_package/identities.py
@@ -27,10 +27,25 @@ from .templates import get_dds_template_path
 
 
 from cryptography import hazmat, x509
-from cryptography.x509.oid import NameOID
+from cryptography.x509.oid import NameOID, ObjectIdentifier
 from cryptography.hazmat.primitives import asymmetric
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization, hashes
+
+
+def parse_dn(dn):
+    """Simple 'Distinguished Name' or RDN parser"""
+    # Other examples
+    # https://github.com/cannatag/ldap3/blob/681e4d8f80032437d4937f29e83f18f6740e6e37/ldap3/utils/dn.py#L274
+    # https://stackoverflow.com/questions/23715944/parsing-x509-distinguished-name
+    parts = []
+    for i in dn.split('/'):
+        for j in i.split(','):
+            if '=' in j:
+                [key, value] = j.split('=', 1)
+                key = key.strip()
+                parts.append([key, value])
+    return dict(parts)
 
 
 class CertificateHelper:
@@ -43,26 +58,43 @@ class CertificateHelper:
 class DDSCertificateHelper:
     """Help build certificate into artifacts."""
 
+    # https://tools.ietf.org/html/rfc4514#page-7
+    _attribute_dict = {
+    'CN': ObjectIdentifier('2.5.4.3'),  # COMMON_NAME
+    'L': ObjectIdentifier('2.5.4.7'),  # LOCALITY_NAME
+    'ST': ObjectIdentifier('2.5.4.8'),  # STATE_OR_PROVINCE_NAME
+    'O': ObjectIdentifier('2.5.4.10'),  # ORGANIZATION_NAME
+    'OU': ObjectIdentifier('2.5.4.11'),  # ORGANIZATION_UNIT_NAME
+    'C': ObjectIdentifier('2.5.4.6'),  # COUNTRY_NAME
+    'STREET': ObjectIdentifier('2.5.4.9'),  # STREET_ADDRESS
+    'DC': ObjectIdentifier('0.9.2342.19200300.100.1.25'),  # DOMAIN_COMPONENT
+    'UID': ObjectIdentifier('0.9.2342.19200300.100.1.1'),  # USER_ID
+    }
+    # https://github.com/pyca/cryptography/blob/master/src/cryptography/x509/oid.py
+    _NAMES_OID = {v: k for k, v in x509.oid._OID_NAMES.items()}
+
+    def dn_dict_to_attributes(self, dn_dict):
+        attributes = []
+        for key, value in dn_dict.items():
+            if key in self._attribute_dict:
+                attribute = x509.NameAttribute(self._attribute_dict[key], value)
+                attributes.append(attribute)
+            else:
+                attribute = x509.NameAttribute(self._NAMES_OID[key], value)
+                attributes.append(attribute)
+        return attributes
+
+
     def build_csr(self, context, csr, dds_key):
-        # FIXME use data in csr xml element to provide subject name
         # OMG Secure DDS 9.4.1.3.2.1 Subject name Section
         # https://tools.ietf.org/html/rfc4514
         hash_algorithm = getattr(hashes, csr.find('hash_algorithm').text)
-        dds_csr = x509.CertificateSigningRequestBuilder().subject_name(x509.Name([
-            # Provide various details about who we are.
-            x509.NameAttribute(NameOID.COUNTRY_NAME, u"US"),
-            x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, u"CA"),
-            x509.NameAttribute(NameOID.LOCALITY_NAME, u"San Francisco"),
-            x509.NameAttribute(NameOID.ORGANIZATION_NAME, u"My Company"),
-            x509.NameAttribute(NameOID.COMMON_NAME, u"mysite.com"),
-            ])).add_extension(
-                x509.SubjectAlternativeName([
-                    # Describe what sites we want this certificate for.
-                    x509.DNSName(u"mysite.com"),
-                    x509.DNSName(u"www.mysite.com"),
-                    x509.DNSName(u"subdomain.mysite.com"),
-                    ]),
-            critical=False,
+        subject_name = csr.find('subject_name').text
+        dn_dict = parse_dn(subject_name)
+        attributes = self.dn_dict_to_attributes(dn_dict)
+
+        dds_csr = x509.CertificateSigningRequestBuilder().subject_name(
+            x509.Name(attributes)
             # Sign the CSR with our private key.
             ).sign(dds_key, hash_algorithm(), default_backend())
         return dds_csr
@@ -72,7 +104,6 @@ class DDSCertificateHelper:
         issuer_name = os.path.normpath(issuer_name)
         ca_key_path = os.path.join(context.private_space, issuer_name + '.pem')
         ca_cert_path = os.path.join(context.public_space, issuer_name + '.pem')
-        print('ca_key_path: ', ca_key_path)
 
         with open(ca_key_path, 'rb') as f:
             ca_key = serialization.load_pem_private_key(
@@ -91,29 +122,23 @@ class DDSCertificateHelper:
 
         subject = dds_csr.subject
 
-        serial_number = int(cert.find('serial_number').text)
-
         validity = cert.find('validity')
         not_before = validity.find('not_before').text
         not_before_datetime = datetime.datetime.strptime(not_before, '%Y-%m-%dT%H:%M:%S')
         not_after = validity.find('not_after').text
         not_after_datetime = datetime.datetime.strptime(not_after, '%Y-%m-%dT%H:%M:%S')
 
-        ca_key, ca_cert = self.get_ca(context, cert.find('issuer_name').text)
+        serial_number = int(cert.find('serial_number').text)
+        issuer_name = cert.find('issuer_name').text
+        ca_key, ca_cert = self.get_ca(context, issuer_name)
 
-        cert_builder = x509.CertificateBuilder().subject_name(
-            subject
-        ).serial_number(
-            serial_number
-        ).not_valid_before(
-            not_before_datetime
-        ).not_valid_after(
-            not_after_datetime
-        ).public_key(
-            dds_csr.public_key()
-        ).issuer_name(
-            ca_cert.subject
-        )
+        cert_builder = x509.CertificateBuilder()
+        cert_builder = cert_builder.subject_name(subject
+            ).serial_number(serial_number
+            ).not_valid_before(not_before_datetime
+            ).not_valid_after(not_after_datetime
+            ).public_key(dds_csr.public_key()
+            ).issuer_name(ca_cert.subject)
         dds_cert = cert_builder.sign(ca_key, hash_algorithm, default_backend())
         return dds_cert
 
@@ -121,6 +146,7 @@ class DDSCertificateHelper:
         dds_certificate_bytes = dds_certificate.public_bytes(
                 encoding=serialization.Encoding.PEM)
         return dds_certificate_bytes
+
 
 class AsymmetricHelper:
     """Help build key into artifacts."""
